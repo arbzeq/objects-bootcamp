3c698bcd53b68873f1333c84d8394fee3efc6bb0
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-07-09 18:31:40 +0200

    Latest

---
+++ objects-bootcamp.js
@@ -0,0 +1,24 @@
+//Bibblan
+
+//1. Skapa ett objekt som innehåller data om en bok, nycklar som kan inkluderas är title, author, numPages.
+let harryPotterBook1 = {
+    title: "Harry Potter: Book1",
+    author: "J. K. Rowling",
+    numPages: 223
+}
+
+//2. Skapa en array av objekt med flera stycken böcker
+let harryPotterBook2 = {
+    title: "Harry Potter: Book2",
+    author: "J. K. Rowling",
+    numPages: 444
+}
+
+
+let harryPotterBook3 = {
+    author: "J. K. Rowling",
+    numPages: 331
+}
+
+let bookArray = [harryPotterBook1, harryPotterBook2, harryPotterBook3];
+console.log(bookArray);
